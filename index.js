1c6ac9eb9d1d25e438ef6b693d101372663e903a
Abubaker <137734278+AbubakerSalah@users.noreply.github.com>
2024-02-20 06:49:14 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const likeBtn = document.getElementById("like-btn")
+const likeClick = document.getElementById("like-click")
+const likeClickGustave = document.getElementById("like-click-gustave")
+const likeClickDucreux = document.getElementById("like-click-ducreux")
 
-likeBtn.addEventListener("click", function() {
-      console.log("hello")
-})
+const likes = document.getElementById("likes")
+const likesGus = document.getElementById("likes-gus")
+const likesDuc = document.getElementById("likes-duc")
+
+let isLiked = false
+
+const handleLikeClick = (likesElement) => {
+    let currentLikes = parseInt(likesElement.textContent.replace(/\D/g, ''), 10)
+    isLiked ? currentLikes-- : currentLikes++
+    likesElement.textContent = `Likes: ${currentLikes}`
+    isLiked = !isLiked
+}
+
+
+
+likeClick.addEventListener("click", () => handleLikeClick(likes))
+likeClickGustave.addEventListener("click", () => handleLikeClick(likesGus))
+likeClickDucreux.addEventListener("click", () => handleLikeClick(likesDuc))
